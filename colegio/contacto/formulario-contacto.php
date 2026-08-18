effd06ea1a28941b8ca270fea5a48b2062debff0
delfinalber <delfin.alber@gmail.com>
2026-08-18 10:54:55 -0500

    pagina-web-18-08-2026-pagina-web-eugenio-ferro-falla

--- colegio/contacto/formulario-contacto.php
+++ colegio/contacto/formulario-contacto.php
@@ -16,7 +16,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $stmt->execute();
     $stmt->close();
     $mysqli->close();
-    header("Location: contacto.html");
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Pragma: no-cache");
+    header("Location: contacto.html", true, 303);
     exit;
 }
 
